77f7cf01cf91d277329add3fd673bb2d36718f6f
Luiz do Valle <lld2131@columbia.edu>
2024-05-30 04:30:04 -0400

    Add script to iteratively find .h and .c files

--- coreutils/extract_deps.sh
+++ coreutils/extract_deps.sh
@@ -8,6 +8,48 @@ OUT_DIR="$PROG_NAME/$C_DIR"
 
 echoerr() { echo "$@" 1>&2;  }
 
+get_headers() {
+    local c_files=("$@")
+    local header_files=()
+    for c_filepath in "${c_files[@]}"; do
+        directory=${c_filepath#./}  # Remove the leading './'
+        directory=${directory%/*}
+        filename=${c_filepath##*/}
+        filename=${filename%%.*}  # Remove extension (e.g. .c and .h)
+        if [ "$directory" = "gnulib/lib" ]; then
+            deps_filepath="./lib/.deps/libcoreutils_a-$filename.Po"
+        elif [ "$directory" = "src" ]; then
+            deps_filepath="./src/.deps/$filename.Po"
+        else
+            echoerr "Warning: File in an unknown directory: '$filepath'"
+            continue
+        fi
+        if [ ! -e "$deps_filepath" ]; then
+            echoerr "Warning: Dependency file $deps_filepath does not exist, skipping..."
+            continue
+        fi
+        readarray -t cur_header_files <<< \
+            "$(grep -E '^[^/][^:]+/[^:]+:$' "$deps_filepath" | sed 's/:$//' | sort -u)"
+                    header_files=( "${header_files[@]}" "${cur_header_files[@]}" )
+    done
+    echo "$(printf "%s\n" "${header_files[@]}" | sort -u)"
+}
+
+get_c_files_from_headers() {
+    local header_files=("$@")
+    local c_files=()
+    for header_file in "${header_files[@]}"; do
+        filename="${header_file##*/}"
+        filename="${filename%.h}"
+        c_file=$(find ./ -type f -name "$filename.c" | head -n 1)
+        if [ -z "$c_file" ]; then
+            continue
+        fi
+        c_files+=( "$c_file" )
+    done
+    echo "$(printf "%s\n" "${c_files[@]}" | sort -u)"
+}
+
 echoerr "Creating the $OUT_DIR directory..."
 rm -rf $PROG_NAME
 mkdir -p $OUT_DIR
@@ -17,8 +59,7 @@ cd $COREUTILS_DIR
 #make -s clean &> /dev/null  # Silence the output
 #make -s $PROG_DIR/$PROG_NAME &> /dev/null  # Silence the output
 
-# Extract the .c files
-echoerr "Copying the .c files to $CORPUS_DIR/$OUT_DIR..."
+echoerr "Collecting all the .h and .c files..."
 c_files=()
 while IFS= read -r line; do
     c_source_file="$(echo "$line" | awk '{print $3}')"
@@ -30,49 +71,28 @@ while IFS= read -r line; do
     c_files+=( "$c_source_filepath" )
 done < <(nm -a src/$PROG_NAME | grep ".*\.c$")
 
-# Extract the .h files
-echoerr "Copying the .h files to $CORPUS_DIR/$OUT_DIR..."
-header_files=()
-for c_filepath in "${c_files[@]}"; do
-    directory=${c_filepath#./}  # Remove the leading './'
-    directory=${directory%/*}
-    filename=${c_filepath##*/}
-    filename=${filename%%.*}  # Remove extension (e.g. .c and .h)
-    if [ "$directory" = "gnulib/lib" ]; then
-        deps_filepath="./lib/.deps/libcoreutils_a-$filename.Po"
-    elif [ "$directory" = "src" ]; then
-        deps_filepath="./src/.deps/$filename.Po"
-    else
-        echoerr "File in an unknown directory: $filepath"
-        exit 1
+new_header_files=($(get_headers "${c_files[@]}"))
+header_files=("${new_header_files[@]}")
+
+i=1
+while [ 1 ]; do
+    echo "Loop $((i++))..."
+    new_c_files=($(get_c_files_from_headers "${new_header_files[@]}"))
+    old_len=${#c_files[@]}
+    c_files=($(printf "%s\n" "${c_files[@]}" "${new_c_files[@]}" | sort -u))
+    if [ ${#c_files[@]} -eq $old_len ]; then
+        break
     fi
-    if [ ! -e "$deps_filepath" ]; then
-        echoerr "Dependency file $deps_filepath does not exist"
-        exit 1
+    new_header_files=($(get_headers "${new_c_files[@]}"))
+    old_len=${#header_files[@]}
+    header_files=($(printf "%s\n" "${header_files[@]}" "${new_header_files[@]}" | sort -u))
+    if [ ${#header_files[@]} -eq $old_len ]; then
+        break
     fi
-    readarray -t cur_header_files <<< \
-        "$(grep -E '^[^/][^:]+/[^:]+:$' "$deps_filepath" | sed 's/:$//' | sort -u)"
-    header_files=( "${header_files[@]}" "${cur_header_files[@]}" )
 done
 
-header_files=($(printf "%s\n" "${header_files[@]}" | sort -u))  # Get unique files
+cp -f "${c_files[@]}" -t "$CORPUS_DIR/$OUT_DIR"
 cp -f "${header_files[@]}" -t "$CORPUS_DIR/$OUT_DIR"
-
-# Include possibly missing .c files
-#
-# Some .h files are included in corresponding .c files which define macros that
-# cause the correct version of the functions to be defined from the #ifndef
-# blocks
-echoerr "Copying the remaining .c files to $CORPUS_DIR/$OUT_DIR..."
-for header_file in "${header_files[@]}"; do
-    filename="${header_file##*/}"
-    filename="${filename%.h}"
-    c_file=$(find ./ -type f -name "$filename.c" | head -n 1)
-    if [ -z "$c_file" ]; then
-        continue
-    fi
-    cp -f "$c_file" -t "$CORPUS_DIR/$OUT_DIR"
-done
 
 # Create the makefile
 echoerr "Creating the Makefile in $CORPUS_DIR/$OUT_DIR..."
